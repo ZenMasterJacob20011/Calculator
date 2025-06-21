6f5b1550a8e8d9964c545e444ff195cdc589d607
Jacob Nichols <jacobnichols@form.io>
2025-06-21 13:45:09 -0500

    draw line on window

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
+    sf::Vertex vertices[]
+    {
+        {{  0.0f,   100.0f}, sf::Color::Red, { 0.0f,  100.0f}},
+        {{  0.0f, 100.0f}, sf::Color::Red, { 0.0f, 100.0f}},
+        {{100.0f, 100.0f}, sf::Color::Red, {10.0f, 10.0f}},
+        {{  0.0f,   100.0f}, sf::Color::Red, { 0.0f,  0.0f}},
+    };
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    window.draw(vertices, 4, sf::PrimitiveType::Lines);
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
